2cee6d3eb8eb9086cdea043c3167346502b62adb
Jared Williams <jared.williams101697@gmail.com>
2024-04-16 14:21:35 -0400

    Connect to api and set up login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
     { path: "", component: HomeComponent },
+    { path: "home/:name", component: HomeComponent },
     { path: "login", component: LoginComponent },
 ];
 
