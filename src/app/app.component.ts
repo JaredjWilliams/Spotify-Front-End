dec0212e674c0b28835ef94a8c9d34546c7bbdbd
jsparks9 <66742639+jsparks9@users.noreply.github.com>
2024-04-16 20:34:41 -0500

    Reverted File Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title: String = "angular-whos-who";
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.createUser();
-  }
-
-  createUser() {
-    this.http.post("http://localhost:8080/users", {
-      username: "test",
-      password: "testing"
-    }).subscribe((data) => {
-      console.log(data);
-    });
-  }
 }
